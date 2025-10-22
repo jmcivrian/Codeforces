9a21bffba33458e6b8b7893abd7fecaf4abe1bab
jmcivrian <jmcivrian@gmail.com>
2025-10-21 19:30:53 -0600

    Accepted 2044A

---
+++ 2044/A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int tests, n;
+    cin >> tests;
+
+    while(tests--)
+    {
+        cin >> n;
+        cout << n-1 << "\n";
+    }
+    return 0;
+}
